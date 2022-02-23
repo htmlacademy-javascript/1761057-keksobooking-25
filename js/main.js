40eff644abd157eb50cd237340c32ce4b7e72114
Valeryia14 <roma6ka_14@mail.ru>
2022-02-23 15:57:23 +0300

    Дополнение к функции

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
 // Функция № 1 (https://stackoverflow.com/questions/62981108/how-does-math-floormath-random-max-min-1-min-work-in-javascript)
 
 const returnRandomInteger = (min, max) => {
-  if (min < max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  if (min < 0 || max < 0) {
+    return -1;
   } else {
+    if (min < max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
     return Math.floor(Math.random() * (min - max + 1) + max);
   }
 };
 
-
 returnRandomInteger(20, 30);
-
 
 // Функция № 2 (https://www.codegrepper.com/code-examples/javascript/Math.random%28%29+toFixed%282%29)
 
 const returnRandomFloat = (min, max, floor) => {
-  if (min < max) {
-    return (Math.random() * (max - min) + min).toFixed(floor);
+  if (min < 0 || max < 0) {
+    return -1;
   } else {
+    if (min < max) {
+      return (Math.random() * (max - min) + min).toFixed(floor);
+    }
     return (Math.random() * (min - max) + max).toFixed(floor);
   }
 };
