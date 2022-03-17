888ebcacd00e7b53b12e1871235d3ee0b0dd08dc
Valeryia14 <roma6ka_14@mail.ru>
2022-03-17 19:49:48 +0300

    Валидация, часть 1

--- js/form.js
+++ js/form.js
@@ -18,4 +18,57 @@ const setInactiveState = () => {
   setDisabledState();
 };
 
+// Валидация
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__item--invalid',
+  successClass: 'ad-form__item--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'ad-form__error'
+});
+
+adForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+  return true;
+});
+
+const roomService = {
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['0']
+};
+
+const guest = adForm.querySelector('#capacity');
+const roomNumber = adForm.querySelector('#room_number');
+const roomValidate = (value) => {
+  if (roomNumber.value.length && roomService[roomNumber.value].includes(value)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const getServiceErrorMessage = () => {
+  switch (roomNumber.value) {
+    case '1':
+      return 'Для 1 гостя';
+    case '2':
+      return 'Для 2 гостей или для 1 гостя';
+    case '3':
+      return 'Для 3 гостей, для 2 гостей или для 1 гостя';
+    case '100':
+      return 'Не для гостей';
+  }
+};
+
+pristine.addValidator(guest, roomValidate, getServiceErrorMessage);
+
+roomNumber.addEventListener('change', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
+
 export {setActiveState, setInactiveState};
